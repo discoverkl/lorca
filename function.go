9514118f2533b93bca6e8792e9c8c6b09598bc43
leokong <leokong@tencent.com>
2019-08-14 13:04:46 +0800

    fix Function.Call

--- function.go
+++ function.go
@@ -16,11 +16,15 @@ func (c *Function) Close() {
 	c.ui.Eval(fmt.Sprintf(`window['%s']['functions'].delete(%d)`, c.BindingName, c.Seq))
 }
 
-func (c *Function) Call(args ...interface{}) error {
-	raw, err := json.Marshal(args)
-	if err != nil {
-		return err
+func (c *Function) Call(args ...interface{}) Value {
+	var jsArg string
+	if args != nil {
+		raw, err := json.Marshal(args)
+		if err != nil {
+			return &value{err: err}
+		}
+		jsArg = fmt.Sprintf(`...%s`, string(raw))
 	}
-	expr := fmt.Sprintf(`window['%s']['functions'].get(%d)(...%s)`, c.BindingName, c.Seq, string(raw))
-	return c.ui.Eval(expr).Err()
+	expr := fmt.Sprintf(`window['%s']['functions'].get(%d)(%s)`, c.BindingName, c.Seq, jsArg)
+	return c.ui.Eval(expr)
 }
